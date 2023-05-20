1c7a453affeaee1ce68f724c7bdcf39e1b6faa45
Luis Durante <luisdurante2@outlook.com>
2023-05-19 23:51:33 -0300

    :white_check_mark: Add unit tests

--- src/questions/questions.controller.spec.ts
+++ src/questions/questions.controller.spec.ts
@@ -1,20 +1,102 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { QuestionsController } from './questions.controller';
 import { QuestionsService } from './questions.service';
+import { ServiceUnavailableException } from '@nestjs/common';
 
 describe('QuestionsController', () => {
-  let controller: QuestionsController;
+  let questionsController: QuestionsController;
+  let questionsService: QuestionsService;
 
-  beforeEach(async () => {
+  const questionsServiceMock = {
+    create: jest.fn(),
+    findAll: jest.fn(),
+  };
+
+  const interactionsMock = [
+    {
+      _id: '6467f63bad9d00dbb4150218',
+      message: 'Message one',
+      answer: 'Answer one',
+    },
+    {
+      _id: '6467f63bad9d00dbb4150219',
+      message: 'Message two',
+      answer: 'Answer two',
+    },
+  ];
+
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [QuestionsController],
-      providers: [QuestionsService],
+      providers: [
+        QuestionsController,
+        {
+          provide: QuestionsService,
+          useValue: questionsServiceMock,
+        },
+      ],
     }).compile();
 
-    controller = module.get<QuestionsController>(QuestionsController);
+    questionsController = module.get<QuestionsController>(QuestionsController);
+    questionsService = module.get<QuestionsService>(QuestionsService);
+  });
+
+  beforeEach(async () => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(questionsController).toBeDefined();
+    expect(questionsService).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should return a created chatgpt interaction', async () => {
+      // Arrange
+      const questionDTOMock = {
+        message: 'Message one',
+      };
+
+      questionsServiceMock.create.mockReturnValue(interactionsMock[0]);
+
+      // Act
+      const interaction = await questionsServiceMock.create(questionDTOMock);
+
+      // Assert
+      expect(interaction).toMatchObject(interactionsMock[0]);
+      expect(questionsService.create).toHaveBeenCalledTimes(1);
+    });
+
+    it('should return a ServiceUnavailableException because chatgpt API is unavailable', async () => {
+      // Arrange
+      const questionDTOMock = {
+        message: 'Message one',
+      };
+
+      questionsServiceMock.create = jest.fn(() => {
+        throw new ServiceUnavailableException();
+      });
+
+      // Act, Assert
+      expect(() => {
+        questionsController.create(questionDTOMock);
+      }).toThrow(ServiceUnavailableException);
+      expect(questionsServiceMock.create).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of created chatgpt interactions', async () => {
+      // Arrange
+      jest
+        .spyOn(questionsService, 'findAll')
+        .mockImplementation(async () => interactionsMock);
+
+      // Act
+      const result = await questionsController.findAll();
+
+      // Assert
+      expect(result.interactions).toHaveLength(2);
+      expect(questionsServiceMock.findAll).toHaveBeenCalledTimes(1);
+    });
   });
 });
--- src/questions/questions.service.spec.ts
+++ src/questions/questions.service.spec.ts
@@ -1,18 +1,130 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { QuestionsService } from './questions.service';
+import { createMock } from '@golevelup/ts-jest';
+import { ChatGptService } from '../chatgpt/chatgpt.service';
+import { Model, Query } from 'mongoose';
+import { InteractionDocument } from './schemas/interaction.schema';
+import { getModelToken } from '@nestjs/mongoose';
+import { ServiceUnavailableException } from '@nestjs/common';
 
 describe('QuestionsService', () => {
-  let service: QuestionsService;
+  let questionsService: QuestionsService;
+  let interactionsModel: Model<InteractionDocument>;
+  let chatGptService: ChatGptService;
 
-  beforeEach(async () => {
+  const interactionsModelMock = {
+    create: jest.fn(),
+    find: jest.fn(),
+  };
+
+  const chatGptServiceMock = {
+    getAnswer: jest.fn(),
+  };
+
+  const interactionsMock = [
+    {
+      _id: '6467f63bad9d00dbb4150218',
+      message: 'Message one',
+      answer: 'Answer one',
+    },
+    {
+      _id: '6467f63bad9d00dbb4150219',
+      message: 'Message two',
+      answer: 'Answer two',
+    },
+  ];
+
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [QuestionsService],
+      providers: [
+        QuestionsService,
+        {
+          provide: getModelToken('Interaction'),
+          useValue: interactionsModelMock,
+        },
+        {
+          provide: ChatGptService,
+          useValue: chatGptServiceMock,
+        },
+      ],
     }).compile();
 
-    service = module.get<QuestionsService>(QuestionsService);
+    questionsService = module.get<QuestionsService>(QuestionsService);
+    interactionsModel = module.get<Model<InteractionDocument>>(
+      getModelToken('Interaction'),
+    );
+    chatGptService = module.get<ChatGptService>(ChatGptService);
+  });
+
+  beforeEach(async () => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(questionsService).toBeDefined();
+    expect(interactionsModel).toBeDefined();
+    expect(chatGptService).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return an array of chatgpt interactions', async () => {
+      // Arrange
+
+      jest.spyOn(interactionsModel, 'find').mockReturnValueOnce(
+        createMock<Query<InteractionDocument[], InteractionDocument>>({
+          exec: jest.fn().mockResolvedValueOnce(interactionsMock),
+        }),
+      );
+
+      // Act
+      const interactions = await questionsService.findAll();
+
+      // Assert
+      expect(interactions).toMatchObject(interactionsMock);
+      expect(interactionsModel.find).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('create interaction', () => {
+    it('should create and return a valid chatgpt interaction', async () => {
+      // Arrange
+      const questionDTOMock = {
+        message: 'Message one',
+      };
+
+      interactionsModelMock.create.mockReturnValue(interactionsMock[0]);
+
+      chatGptServiceMock.getAnswer.mockReturnValue({
+        choices: [{ message: { content: 'Answer one' } }],
+      });
+
+      // Act
+      const interaction = await questionsService.create(questionDTOMock);
+
+      // Assert
+      expect(interaction).toMatchObject(interactionsMock[0]);
+      expect(interactionsModel.create).toHaveBeenCalledTimes(1);
+      expect(chatGptService.getAnswer).toHaveBeenCalledTimes(1);
+    });
+
+    it('should return a ServiceUnavailableException because chatgpt API is unavailable', async () => {
+      // Arrange
+      const questionDTOMock = {
+        message: 'Message one',
+      };
+
+      interactionsModelMock.create.mockReturnValue(interactionsMock[0]);
+
+      chatGptServiceMock.getAnswer = jest.fn(() => {
+        throw new ServiceUnavailableException();
+      });
+
+      // Act, Assert
+      await expect(questionsService.create(questionDTOMock)).rejects.toThrow(
+        ServiceUnavailableException,
+      );
+      expect(chatGptService.getAnswer).toHaveBeenCalledTimes(1);
+      expect(interactionsModel.create).toHaveBeenCalledTimes(0);
+    });
   });
 });
